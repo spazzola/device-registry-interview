f53a34dd087c55f89c88af6fff7955d0a24504ec
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 19:40:38 +0200

    Refactor - migrated validation to private function

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -7,13 +7,21 @@ class ReturnDeviceFromUser
   def call
     device = Device.find_by(serial_number: @serial_number)
 
-    if device.nil? || device.user_id != @user.id
-      raise ReturnError::Unauthorized
-    end
+    check_if_can_return(device)
+    
     device.user = nil
     device.save!
 
     device
   end
   
+
+  private
+
+  def check_if_can_return(device)
+    if device.nil? || device.user_id != @user.id
+      raise ReturnError::Unauthorized
+    end
+  end
+
 end
