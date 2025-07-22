35d4005214c064359aebd330ee72a8ee40f48a10
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 08:20:13 +0200

    Added constructor implementation

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -2,6 +2,9 @@
 
 class AssignDeviceToUser
   def initialize(requesting_user:, serial_number:, new_device_owner_id:)
+    @requesting_user = requesting_user
+    @serial_number = serial_number
+    @new_device_owner_id = new_device_owner_id
   end
 
   def call
