ec99a715a94cded8ac4c759970b86b6aa38cb6e8
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 08:46:53 +0200

    Refactor - extracted validation and creating device to private functions

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -10,6 +10,16 @@ class AssignDeviceToUser
   def call
     device = Device.find_or_initialize_by(serial_number: @serial_number)
 
+    validate_device(device);
+    create_device_object(device);
+
+    device
+  end
+
+
+  private
+
+  def validate_device(device)
     if device.persisted?
       if device.user_id && device.user_id != @requesting_user.id
         #TODO throw an error
@@ -19,11 +29,11 @@ class AssignDeviceToUser
         #TODO throw an error
       end
     end
+  end
 
+  def create_device_object(device)
     device.user_id = @requesting_user.id
     device.returned_by_id = nil
     device.save!
-
-    device
   end
 end
