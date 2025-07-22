df244710fb4061b7119639cdcbcf6ed688eb3d72
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 19:24:54 +0200

    Added test to check if device is sucessfuly unassigned from user

--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe ReturnDeviceFromUser do
       new_device_owner_id: user.id
     ).call
   end
+
+  context 'returns device successfully' do
+    before do
+      ReturnDeviceFromUser.new(
+        user: user,
+        serial_number: serial_number
+      ).call
+    end
+
+    it 'returns device and unassign user' do
+      expect(@device.serial_number).to eq(serial_number)
+      expect(@device.user).to eq(nil)
+    end
+  end
+
 end
