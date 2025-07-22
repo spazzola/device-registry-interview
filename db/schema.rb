590f38460a53f98a987cddbc01771e78eb7917bd
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 22:35:20 +0200

    Added missing schema.rb for ReturnsHistory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_22_170243) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_22_182313) do
   create_table "api_keys", force: :cascade do |t|
     t.integer "bearer_id", null: false
     t.string "bearer_type", null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_22_170243) do
     t.index ["user_id"], name: "index_devices_on_user_id"
   end
 
+  create_table "returns_histories", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "device_id", null: false
+    t.datetime "assigned_at"
+    t.datetime "returned_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["device_id"], name: "index_returns_histories_on_device_id"
+    t.index ["user_id"], name: "index_returns_histories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest", null: false
@@ -41,4 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_22_170243) do
 
   add_foreign_key "devices", "users"
   add_foreign_key "devices", "users", column: "returned_by_id"
+  add_foreign_key "returns_histories", "devices"
+  add_foreign_key "returns_histories", "users"
 end
