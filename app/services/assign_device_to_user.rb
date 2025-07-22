ef4b70b49fb16ceaa4b234a5788417734036b7a3
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 08:34:03 +0200

    Added basic implementation of call function

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -8,5 +8,22 @@ class AssignDeviceToUser
   end
 
   def call
+    device = Device.find_or_initialize_by(serial_number: @serial_number)
+
+    if device.persisted?
+      if device.user_id && device.user_id != @requesting_user.id
+        #TODO throw an error
+      end
+
+      if device.returned_by_id == @requesting_user.id
+        #TODO throw an error
+      end
+    end
+
+    device.user_id = @requesting_user.id
+    device.returned_by_id = nil
+    device.save!
+
+    device
   end
 end
