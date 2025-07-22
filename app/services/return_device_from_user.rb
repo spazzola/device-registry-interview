9c5d00ca7758aaa0ea4559246a3587f7ee780de6
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-22 19:37:49 +0200

    Added validation if user tries to return device assigned to another user. Added checking if device is nil

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -7,6 +7,9 @@ class ReturnDeviceFromUser
   def call
     device = Device.find_by(serial_number: @serial_number)
 
+    if device.nil? || device.user_id != @user.id
+      raise ReturnError::Unauthorized
+    end
     device.user = nil
     device.save!
 
