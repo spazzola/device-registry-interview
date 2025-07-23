2793dc6a38517834a9492a6bc535ac80ccbb5a8f
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-23 16:02:46 +0200

    Fixed wrong syntax for validate User Record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates: @email, presense: true, unique: true
-  
+  validates :email, presense: true, uniqueness: true
+
   has_many :api_keys, as: :bearer
   has_many :devices
   has_many :returns_histories
