89494498546e30058f93d13e2feb493773a4d6c3
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-23 16:06:44 +0200

    Fixed typo in validates statement for Device Record

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,5 @@
 class Device < ApplicationRecord
-  validates :serial_number, presense: true, uniqueness: true
+  validates :serial_number, presence: true, uniqueness: true
 
   belongs_to :user, optional: true
   has_many :returns_histories
