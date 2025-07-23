6430f599c66209e1abe6030f60ce31da5843ad95
Rafal Szczotka <rafalszczotka@wp.pl>
2025-07-23 16:02:53 +0200

    Fixed wrong syntax for validate Device Record

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,6 @@
 class Device < ApplicationRecord
-  validates: @serial_number, presense: true, unique: true
+  validates :serial_number, presense: true, uniqueness: true
+
   belongs_to :user, optional: true
   has_many :returns_histories
 end
